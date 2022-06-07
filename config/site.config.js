965c61de79cde1b8965d4d308ccba292ecac0ceb
Angel Ramirez <65986791+angelrsz2899@users.noreply.github.com>
2022-06-07 07:58:24 -0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Animes', '/Películas', '/Series Animadas', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/animedtup/All Projects', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
