6050768dac90fba49817c0a04556ed3b0de2c26f
Angel Ramirez <65986791+angelrsz2899@users.noreply.github.com>
2022-08-19 23:05:59 -0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "animedtup",
+  title: "TKArruti",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Lo-Rescatable',
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'Discord',
-      link: 'https://discord.gg/H96mjnKNxh',
+      link: '#',
     },
   ],
 
